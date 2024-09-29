04edda748e839b8c157a204483530d7383203edf
rizzabh <pandeyrishabh966@gmail.com>
2024-09-29 22:33:41 +0530

    sidebar fix

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,49 +1,88 @@
 import * as Icons from "lucide-react";
 import React from "react";
 import { Tooltip } from "react-tooltip";
+import { BsBoxSeam } from "react-icons/bs";
+import { RxDashboard } from "react-icons/rx";
+import { GiDiamonds } from "react-icons/gi";
+import { RiFunctionAddLine } from "react-icons/ri";
+import { useLocation } from 'react-router-dom';
+
+
+
 
 const Sidebar = () => {
   const Menus = [
     { title: "/dashboard", src: "Layout", path: "/dashboard" },
     { title: "/crates", src: "LayoutGrid", path: "/explorecrate" },
     { title: "/scroll", src: "Sparkle", path: "/scroll" },
-    { title: "/create Crate", src: "PackageOpen", path: "/cratecreator" },
+    { title: "/cratecreator", src: "PackageOpen", path: "/cratecreator" },
   ];
+
+  //href, logo(react element) , path
+
+  //take pathname from router and store it in path
+
+  const location = useLocation();
+
+
+  
+
+  const sidebarData = [
+    {
+      path: "/dashboard",
+      logo: <BsBoxSeam className={`${location.pathname === "/dashboard" ? "text-[#B6FF1B]" :"" }`} /> 
+    },
+    {
+      path: "/explorecrate",
+      logo: <RxDashboard className={`${location.pathname === "/explorecrate" ? "text-[#B6FF1B]" :"" }`} />
+
+    },
+    {
+      path: "/scroll",
+      logo: <GiDiamonds className={`${location.pathname === "/scroll" ? "text-[#B6FF1B]" :"" }`} />
+
+    },
+    {
+      path: "/cratecreator",
+      logo: <RiFunctionAddLine className={`${location.pathname === "/cratecreator" ? "text-[#B6FF1B]" :"" } `}/>
+    }
+
+  ]
 
   return (
     <div className="fixed left-0 top-0 h-full z-40 hidden md:block">
       <div 
         className={`
-          flex flex-col justify-between h-full bg-black text-yellow-300 transition-all duration-300 ease-in-out
+          flex flex-col justify-between h-full bg-gradient-to-b from-[#0A1019] to-[#02050A] text-gray-400 transition-all duration-300 ease-in-out
           w-16 sm:w-20
         `}
       >
         <div>
           <div className="flex items-center justify-center h-16 sm:h-20 mb-4 sm:mb-8">
             <img
-              src="/images/SICK_LOGO2.png"
+              src="/sickLogo.png"
               alt="Logo"
-              className="w-12 h-12 sm:w-16 sm:h-16 cursor-pointer transition-all duration-300 ease-in-out hover:scale-110"
+              className="w-10 h-10 cursor-pointer transition-all duration-300 ease-in-out hover:scale-110"
             />
           </div>
           <nav>
-            <ul className="space-y-2 sm:space-y-4 px-2 sm:px-3">
-              {Menus.map((menu, index) => {
-                const Icon = Icons[menu.src as keyof typeof Icons] as React.ElementType;
+            <ul className="px-4 space-y-4">
+              {sidebarData.map((menu, index) => {
+                
                 return (
                   <li key={index}>
                     <a
                       href={menu.path}
                       data-tooltip-id={`tooltip-${index}`}
-                      data-tooltip-content={menu.title}
-                      className="flex items-center justify-center p-2 sm:p-3 rounded-full sm:rounded-3xl transition-all duration-200 ease-in-out group"
+                      data-tooltip-content={menu.path}
+                      className="flex items-center border border-gray-600 justify-center p-3 rounded-full sm:rounded-3xl transition-all duration-200 text-xl ease-in-out group"
                     >
-                      {Icon && <Icon className="text-yellow-400 group-hover:text-yellow-300" size={20} />}
+                      {menu.logo}
                     </a>
                     <Tooltip
                       id={`tooltip-${index}`}
                       place="right"
-                      className="!bg-yellow-500 !text-black font-mono"
+                      className="bg-black !text-[#B6FF1B] font-mono"
                     />
                   </li>
                 );
@@ -52,11 +91,11 @@ const Sidebar = () => {
           </nav>
         </div>
         <div className="px-2 sm:px-3 mb-4 sm:mb-8 space-y-2 sm:space-y-4">
-          <button className="w-full flex items-center justify-center p-2 sm:p-3 rounded-full sm:rounded-lg transition-all duration-200 ease-in-out hover:bg-yellow-500 hover:bg-opacity-20 group">
-            <Icons.Gem className="text-yellow-400 group-hover:text-yellow-300" size={20} />
+          <button className="w-full flex items-center justify-center p-2 sm:p-3 rounded-full sm:rounded-lg transition-all duration-200 ease-in-out hover:bg-green-500 hover:bg-opacity-20 group">
+            <Icons.Gem className="text-[#B6FF1B] " size={20} />
           </button>
-          <button className="w-full flex items-center justify-center p-2 sm:p-3 rounded-full sm:rounded-lg transition-all duration-200 ease-in-out hover:bg-yellow-500 hover:bg-opacity-20 group">
-            <Icons.LogOut className="text-yellow-400 group-hover:text-yellow-300" size={20} />
+          <button className="w-full flex items-center justify-center p-2 sm:p-3 rounded-full sm:rounded-lg transition-all duration-200 ease-in-out hover:bg-red-500 hover:bg-opacity-20 group">
+            <Icons.LogOut className="text-gray-400 group-hover:text-red-300" size={20} />
           </button>
         </div>
       </div>
--- src/components/ui/sidebarPhone.tsx
+++ src/components/ui/sidebarPhone.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import * as Icons from "lucide-react";
+import { useLocation } from 'react-router-dom';
+import { BsBoxSeam } from "react-icons/bs";
+import { RxDashboard } from "react-icons/rx";
+import { GiDiamonds } from "react-icons/gi";
+import { RiFunctionAddLine } from "react-icons/ri";
 
 export const SideBarPhone = () => {
   const Menus = [
@@ -9,20 +14,62 @@ export const SideBarPhone = () => {
     { title: "Create Crate", src: "PackageOpen", path: "/cratecreator" },
   ];
 
+  
+  const location = useLocation();
+
+
+  
+
+  const sidebarData = [
+    {
+      path: "/dashboard",
+      logo: <BsBoxSeam className={`${location.pathname === "/dashboard" ? "text-[#B6FF1B]" :"" }`} />,
+      name: <p className={`${location.pathname === "/dashboard" ? "text-[#B6FF1B] text-xs" :"text-xs" }`}>{location.pathname === "/dashboard" ? "/": ""}dashboard </p>
+
+    },
+    {
+      path: "/explorecrate",
+      logo: <RxDashboard className={`${location.pathname === "/explorecrate" ? "text-[#B6FF1B]" :"" }`} />,
+      name: <p className={`${location.pathname === "/explorecrate" ? "text-[#B6FF1B] text-xs" :"text-xs" }`}>{location.pathname === "/explorecrate" ? "/": ""}explore-crate </p>
+
+
+    },
+    {
+      path: "/scroll",
+      logo: <GiDiamonds className={`${location.pathname === "/scroll" ? "text-[#B6FF1B]" :"" }`} />,
+      name: <p className={`${location.pathname === "/scroll" ? "text-[#B6FF1B] text-xs" :"text-xs" }`}>{location.pathname === "/scroll" ? "/": ""}scroll </p>
+
+
+    },
+    {
+      path: "/cratecreator",
+      logo: <RiFunctionAddLine className={`${location.pathname === "/cratecreator" ? "text-[#B6FF1B]" :"" } `}/>,
+      name: <p className={`${location.pathname === "/cratecreator" ? "text-[#B6FF1B] text-xs" :"text-xs" }`}>{location.pathname === "/cratecreator" ? "/": ""}crate-creator </p>
+
+    }
+  ]
+
   return (
-    <div className="block md:hidden fixed bottom-0 left-0 right-0 bg-black z-50">
-      <nav className="flex justify-around items-center h-16 px-4">
-        {Menus.map((menu, index) => {
-          const Icon = Icons[menu.src as keyof typeof Icons] as React.ElementType;
+    <div className="block md:hidden fixed bottom-0 left-0 right-0 bg-[#02050ac0] backdrop-blur-md  z-50">
+      <nav className="flex justify-around items-center h-20 px-4">
+        {sidebarData.map((menu, index) => {
           return (
-            <a
-              key={index}
-              href={menu.path}
-              className="flex flex-col items-center justify-center text-yellow-300 hover:text-yellow-400 transition-colors duration-200"
-            >
-              {Icon && <Icon size={20} />}
-              <span className="text-xs mt-1">{menu.title}</span>
-            </a>
+            <li key={index} className='list-none flex-col text-center'>
+                    <a
+                      href={menu.path}
+                      data-tooltip-id={`tooltip-${index}`}
+                      data-tooltip-content={menu.path}
+                      className="flex items-center justify-center p-3 rounded-full sm:rounded-3xl transition-all duration-200 text-xl ease-in-out group"
+                    >
+                      {menu.logo}
+                      
+                      
+                    </a>
+                    {menu.name}
+
+
+                    
+                  </li>
           );
         })}
       </nav>
