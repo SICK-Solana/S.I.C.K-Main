7ee73c88f5103f76481ff1c9d42941075726c9b0
Jay Singh <140599484+mathdebate09@users.noreply.github.com>
2024-10-04 21:31:00 +0530

    feat: pwa functionality

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,46 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'S.I.C.K.',
+        short_name: 'S.I.C.K.',
+        description: 'You can Create SIPs called "Crates" and get acknowledged with a Social angle',
+        theme_color: '#0A1019',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-1080.png',
+            sizes: '1080x1080',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     proxy: {
       '/api': {
         target: 'https://sickb.vercel.app',
         changeOrigin: true,
         secure: false,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
