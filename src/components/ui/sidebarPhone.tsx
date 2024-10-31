ae1ab63375676c415e7f290e293a930239f4f598
rizzabh <pandeyrishabh966@gmail.com>
2024-10-31 13:58:12 +0530

    return calculator

--- src/components/ui/sidebarPhone.tsx
+++ src/components/ui/sidebarPhone.tsx
@@ -66,8 +66,6 @@ export const SideBarPhone = () => {
                       
                     </a>
                     {menu.name}
-
-
                     
                   </li>
           );
